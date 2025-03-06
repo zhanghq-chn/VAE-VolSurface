903a1f668b12e3a11d4dd3e840feaba6882a7c07
zhanghq-mbp2023 <zhanghq.chn@gmail.com>
2025-03-05 21:47:30 -0500

    Add sql version control

---
+++ db_versioning/schema.sql
@@ -0,0 +1,90 @@
+CREATE TABLE IF NOT EXISTS opprc (
+    secid INTEGER,
+    date TEXT,
+    symbol TEXT,
+    symbol_flag INTEGER,
+    exdate TEXT,
+    last_date TEXT,
+    cp_flag TEXT,
+    strike_price INTEGER,
+    best_bid REAL,
+    best_offer REAL,
+    volume INTEGER,
+    open_interest INTEGER,
+    impl_volatility REAL,
+    delta REAL,
+    gamma REAL,
+    vega REAL,
+    theta REAL,
+    optionid INTEGER,
+    cfadj INTEGER,
+    am_settlement INTEGER,
+    contract_size INTEGER,
+    ss_flag INTEGER,
+    forward_price REAL,
+    expiry_indicator INTEGER,
+    root TEXT,
+    suffix TEXT,
+    cusip TEXT,
+    ticker TEXT,
+    sic INTEGER,
+    index_flag INTEGER,
+    exchange TEXT,
+    class TEXT,
+    issue_type TEXT,
+    industry_group TEXT,
+    issuer TEXT,
+    div_convention TEXT,
+    exercise_style TEXT,
+    am_set_flag TEXT
+);
+
+CREATE TABLE IF NOT EXISTS secprc (
+    secid INTEGER,
+    date TEXT,
+    cusip TEXT,
+    ticker TEXT,
+    sic INTEGER,
+    index_flag INTEGER,
+    exchange TEXT,
+    class TEXT,
+    issue_type TEXT,
+    industry_group TEXT,
+    low REAL,
+    high REAL,
+    open REAL,
+    close REAL,
+    volume INTEGER,
+    return REAL,
+    cfadj INTEGER,
+    shrout INTEGER,
+    cfret INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS fwdprc (
+    secid INTEGER,
+    date TEXT,
+    expiration TEXT,
+    am_settlement INTEGER,
+    forward_price REAL,
+    cusip TEXT,
+    ticker TEXT,
+    sic INTEGER,
+    index_flag INTEGER,
+    exchange TEXT,
+    class TEXT,
+    issue_type TEXT,
+    industry_group TEXT,
+    issuer TEXT
+);
+
+CREATE TABLE IF NOT EXISTS rate (
+    date TEXT, 
+    days INTEGER, 
+    rate REAL
+);
+
+CREATE INDEX idx_op_date on opprc (date);
+CREATE INDEX idx_sec_date on secprc (date);
+CREATE INDEX idx_op_strike on opprc (strike_price);
+
---
+++ db_versioning/seed.sql
@@ -0,0 +1,5 @@
+.mode csv
+.import --skip 1 optionm_spx_opprc_20220831-20230831.csv opprc
+.import --skip 1 optionm_spx_secpr_20220831-20230831.csv secprc
+.import --skip 1 optionm_spx_fwdpr_20220831-20230831.csv fwdprc
+.import --skip 1 optionm_zerocd_20220831-20230831.csv rate
