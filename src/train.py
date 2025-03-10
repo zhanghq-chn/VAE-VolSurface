28ae9e8553bf6f6970f4ce3b9f5add3e880869a0
Mikamkan <siqingzou1@gmail.com>
2025-03-10 16:58:51 -0400

    Enable hypertune

--- src/train.py
+++ src/train.py
@@ -4,6 +4,10 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune import CLIReporter
+
 import argparse
 
 
@@ -32,50 +36,67 @@ class Trainer(object):
 
         self.network_param = self.config["network"]
 
+        self.hypertune_param = self.config["hypertune"]
+
         # other
         self.model_type = self.config["model"]["type"]
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def create_model(self):
+    #### MODEL
+    # set config here only doing hypertune
+    def create_model(self, hyper_config=None):
+        network_param = hyper_config if hyper_config else self.network_param
+        train_param = hyper_config if hyper_config else self.train_param
         if self.model_type == "vae":
             # check params
             for key in ["input_dim", "hidden_dim", "latent_dim"]:
                 assert (
-                    key in self.network_param
+                    key in network_param
                 ), f"Key '{key}' is missing in the network params."
 
             self.model = vae.VAE(
-                self.network_param["input_dim"],
-                self.network_param["hidden_dim"],
-                self.network_param["latent_dim"],
+                network_param["input_dim"],
+                network_param["hidden_dim"],
+                network_param["latent_dim"],
             ).to(self.device)
-            self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+            self.optimizer = optim.Adam(
+                self.model.parameters(), lr=train_param["learning_rate"]
+            )
 
+        # add other model types here
         else:
             print("Model not found")
             return
 
+    #### MODEL TRAIN
     def load_model(self, path):
         self.model.load_state_dict(torch.load(path))
         print(f"Model loaded from {path}")
 
-    def train(self, train_loader: DataLoader):
+    def train(self, train_loader: DataLoader, echo=True):
         self.model.train()
         train_loss = 0
         for batch_idx, (data, _) in enumerate(train_loader):
             data = data.view(-1, self.network_param["input_dim"]).to(self.device)
             self.optimizer.zero_grad()
+
             if self.model_type == "vae":
                 x_recon, mean, logvar = self.model(data)
                 loss = vae.loss_function(x_recon, data, mean, logvar)
+
+            # Add other model types here
             else:
                 print("Model not found")
                 return
+
             loss.backward()
             train_loss += loss.item()
             self.optimizer.step()
-
-        print(f"Epoch {epoch+1}, Loss: {train_loss / len(train_loader.dataset):.4f}")
+        if echo:
+            print(
+                f"Epoch {epoch+1}, Loss: {train_loss / len(train_loader.dataset):.4f}"
+            )
+        return train_loss
 
     def evaluate(self, output_path):
         self.model.eval()
@@ -84,23 +105,90 @@ class Trainer(object):
             sample = self.model.decoder(sample).cpu()
             save_image(sample.view(64, 1, 28, 28), f"{output_path}/sample.png")
 
+    #### HYPERTUNE
+    # create hypertune config from yaml
+    @staticmethod
+    def make_hypertune_config(raw_config: dict):
+        hyper_config = {key: tune.choice(value) for key, value in raw_config.items()}
+        return hyper_config
+
+    def hyper_train(self, train_loader: DataLoader, hyper_config: dict):
+        assert hyper_config, "Hyperparameter config is not set."
+        self.create_model(hyper_config)
+        for epoch in range(self.epochs):
+            ###### here logic change for different dataset (eg:batchsize should be included) ######
+            loss = self.train(train_loader)
+            tune.report(loss=loss)
+
+    def hypertune(self, train_loader: DataLoader):
+        reporter = CLIReporter(metric_columns=["loss", "training_iteration"])
+        scheduler = ASHAScheduler(metric="loss", mode="min")
+
+        # Run the grid search
+        analysis = tune.run(
+            lambda config: self.hyper_train(train_loader, config),
+            config=self.make_hypertune_config(self.hypertune_param),
+            resources_per_trial={
+                "cpu": 2
+            },  # Allocate resources ---> shoule be available in yaml
+            num_samples=1,  # Number of samples per configuration
+            scheduler=scheduler,
+            progress_reporter=reporter,
+            verbose=1,
+        )
+
+        print("Best config:", analysis.get_best_config(metric="loss", mode="min"))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="vae_v1", help="Model name")
+    ## Train
+    parser.add_argument("--train", type=bool, default=False, help="Train model")
     parser.add_argument("--load", type=bool, default=False, help="Load model")
     parser.add_argument("--save", type=bool, default=False, help="Save model")
+
+    ## Hyperparameter tuning
+    parser.add_argument(
+        "--hypertune", type=bool, default=False, help="Hyperparameter tuning"
+    )
 
     args = parser.parse_args()
 
     trainer = Trainer(args.model)
-    trainer.create_model()
 
-    if args.load:
-        trainer.load_model(f"params/{trainer.model_name}.pth")
-    else:
-        print("Training from scratch")
+    if args.train:
+        trainer.create_model()
 
+        if args.load:
+            trainer.load_model(f"params/{trainer.model_name}.pth")
+        else:
+            print("Training from scratch")
+
+            # create dataset
+            transform = transforms.Compose([transforms.ToTensor()])
+            train_dataset = datasets.MNIST(
+                root="./data", train=True, transform=transform, download=True
+            )
+            train_loader = DataLoader(
+                dataset=train_dataset, batch_size=trainer.batch_size, shuffle=True
+            )
+
+            # train
+            for epoch in range(trainer.epochs):
+                trainer.train(train_loader)
+
+            if args.save:
+                torch.save(
+                    trainer.model.state_dict(), f"params/{trainer.model_name}.pth"
+                )
+
+            print("Training complete.")
+
+        # evaluate
+        trainer.evaluate("output")
+
+    if args.hypertune:
         # create dataset
         transform = transforms.Compose([transforms.ToTensor()])
         train_dataset = datasets.MNIST(
@@ -110,14 +198,5 @@ if __name__ == "__main__":
             dataset=train_dataset, batch_size=trainer.batch_size, shuffle=True
         )
 
-        # train
-        for epoch in range(trainer.epochs):
-            trainer.train(train_loader)
-
-        if args.save:
-            torch.save(trainer.model.state_dict(), f"params/{trainer.model_name}.pth")
-
-        print("Training complete.")
-
-    # evaluate
-    trainer.evaluate("output")
+        # hypertune
+        trainer.hypertune(train_loader)
