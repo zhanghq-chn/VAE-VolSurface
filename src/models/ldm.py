3858f85e3636bbb429680f4a13c9ed067a4446b9
Mikamkan <siqingzou1@gmail.com>
2025-03-18 23:34:56 -0400

    ldm draft

--- src/models/ldm.py
+++ src/models/ldm.py
@@ -1,30 +1,26 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
 import numpy as np
 
-from src.models import vae
 
 class NoisePredictor(nn.Module):
     def __init__(self, latent_dim, hidden_dim):
         super(NoisePredictor, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(latent_dim + 1, hidden_dim), # add t to predict noise
+            nn.Linear(latent_dim + 1, hidden_dim),  # add t to predict noise
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, latent_dim),
         )
-        
+
     def forward(self, z, t_norm):
         #### FIX
-        t_embed = torch.sin(2*np.pi*t_norm).unsqueeze(1).to(z.device)
+        t_embed = torch.sin(2 * np.pi * t_norm).unsqueeze(1).to(z.device)
         z_t = torch.cat([z, t_embed], dim=1)
         # predict noise
         return self.model(z_t)
+
 
 class LDM(nn.module):
     def __init__(self, autoencoder, noise_predictor):
@@ -32,13 +28,13 @@ class LDM(nn.module):
         # default vae model
         self.autoencoder = autoencoder
         self.noise_predictor = noise_predictor
-    
+
     def forward(self, x, t):
         # encode input into latent space
         mean, logvar = self.autoencoder.encoder(x)
         noise_pred = self.noise_predictor(mean, t)
         return noise_pred
-    
+
     # Diffusion Process Utilities
     @staticmethod
     def linear_beta_schedule(timesteps):
