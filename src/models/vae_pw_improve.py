64a49d32b0c7cb5b788968ab084de344041c3b5b
zhanghq-mbp2023 <zhanghq.chn@gmail.com>
2025-04-18 10:28:58 -0400

    Fix typo in VAE PW

--- src/models/vae_pw_improve.py
+++ src/models/vae_pw_improve.py
@@ -12,13 +12,14 @@ class VAE_PW_II(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim):
         super(VAE_PW_II, self).__init__()
         self.encoder = Encoder(input_dim, hidden_dim, latent_dim)
-        self.decoder = Decoder(latent_dim + 2, hidden_dim, 1)
+        self.decoder = Decoder(latent_dim, hidden_dim, 1)
         self.dltembed = SinusoidalPositionalEmbedding(10)
         self.ttmembed = SinusoidalPositionalEmbedding(10)
         self.dltemb_net = EmbeddingMLP(10, latent_dim)
         self.ttmemb_net = EmbeddingMLP(10, latent_dim)
 
-    def reparameterize(self, mean, logvar):
+    @staticmethod
+    def reparameterize(mean, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mean + eps * std
